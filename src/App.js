c4e39531901f339d8b17fe2ee480c455cca7173e
Steven <stevengf14@gmail.com>
2022-04-04 11:28:14 -0500

    Refactoring our DisplayBalance React Component

--- src/App.js
+++ src/App.js
@@ -2,35 +2,21 @@ import './App.css';
 import { Container, Grid, Segment, Statistic, Icon } from 'semantic-ui-react';
 import MainHeader from './components/MainHeader';
 import NewEntryForm from './components/NewEntryForm';
+import DisplayBalance from './components/DisplayBalance';
 
 function App() {
   return (
     <Container>
       <MainHeader title="Budget" type="h1" />
-
-      <Statistic size="small">
-        <Statistic.Label>Your Balance: </Statistic.Label>
-        <Statistic.Value>2,550.53</Statistic.Value>
-      </Statistic>
-
+      <DisplayBalance title="Your Balance" value="2,550.53" size="small" />
       <Segment textAlign="center">
         <Grid columns={2}>
           <Grid.Row>
             <Grid.Column>
-              <Statistic size="tiny" color="green">
-                <Statistic.Label style={{ textAlign: "left" }}>
-                  Incoming:
-                </Statistic.Label>
-                <Statistic.Value>1,045.50</Statistic.Value>
-              </Statistic>
+              <DisplayBalance title="Income" value="1,045.50" color="green" />
             </Grid.Column>
             <Grid.Column>
-              <Statistic size="tiny" color="red">
-                <Statistic.Label style={{ textAlign: "left" }}>
-                  Expenses:
-                </Statistic.Label>
-                <Statistic.Value>623.50</Statistic.Value>
-              </Statistic>
+              <DisplayBalance title="Expenses" value="623.50" color="red" />
             </Grid.Column>
           </Grid.Row>
         </Grid>
