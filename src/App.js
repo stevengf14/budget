b3d72c464f48342f46a2ce511fba41fae18d5a27
Steven <stevengf14@gmail.com>
2022-04-04 11:40:45 -0500

    Creating our EntryLine React Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import MainHeader from './components/MainHeader';
 import NewEntryForm from './components/NewEntryForm';
 import DisplayBalance from './components/DisplayBalance';
 import DisplayBalances from './components/DisplayBalances';
+import EntryLine from './components/EntryLine';
 
 function App() {
   return (
@@ -12,41 +13,9 @@ function App() {
       <DisplayBalance title="Your Balance" value="2,550.53" size="small" />
       <DisplayBalances />
       <MainHeader title="History" type="h3" />
-      <Segment color="red">
-        <Grid columns={3} textAlign="right">
-          <Grid.Row>
-            <Grid.Column columns={10} textAlign="left">Something</Grid.Column>
-            <Grid.Column columns={3} textAlign="right">$10.00</Grid.Column>
-            <Grid.Column columns={3}>
-              <Icon name="edit" bordered />
-              <Icon name="trash" bordered />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-      <Segment color="red">
-        <Grid columns={3} textAlign="right">
-          <Grid.Row>
-            <Grid.Column columns={10} textAlign="left">Something else</Grid.Column>
-            <Grid.Column columns={3} textAlign="right">$20.00</Grid.Column>
-            <Grid.Column columns={3}>
-              <Icon name="edit" bordered />
-              <Icon name="trash" bordered />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment><Segment color="blue">
-        <Grid columns={3} textAlign="right">
-          <Grid.Row>
-            <Grid.Column columns={10} textAlign="left">Something important</Grid.Column>
-            <Grid.Column columns={3} textAlign="right">$100.00</Grid.Column>
-            <Grid.Column columns={3}>
-              <Icon name="edit" bordered />
-              <Icon name="trash" bordered />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
+      <EntryLine color="red" description="Income" value="$15.00" />
+      <EntryLine color="red" description="Another income" value="$10.00" />
+      <EntryLine color="red" description="Expense" value="$20.00" isExpense/>
 
       <MainHeader title="Add new Transaction" type="h3" />
       <NewEntryForm />
---
+++ src/components/EntryLine.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Grid, Segment, Icon } from 'semantic-ui-react';
+
+function EntryLine({ description, value, isExpense = false}) {
+    return (
+        <Segment color={isExpense ? "red": "green"}>
+            <Grid columns={3} textAlign="right">
+                <Grid.Row>
+                    <Grid.Column columns={10} textAlign="left">{description}</Grid.Column>
+                    <Grid.Column columns={3} textAlign="right">{value}</Grid.Column>
+                    <Grid.Column columns={3}>
+                        <Icon name="edit" bordered />
+                        <Icon name="trash" bordered />
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </Segment>
+    );
+} export default EntryLine;
