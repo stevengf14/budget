74cb2fd76fa4a1a74a507f6638f178b3632235ae
Steven <stevengf14@gmail.com>
2022-04-08 16:13:41 -0500

    Creating a model to edit the entries

--- src/components/EntryLine.js
+++ src/components/EntryLine.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import { Grid, Segment, Icon } from 'semantic-ui-react';
+import ModalEdit from "./ModalEdit";
 
 function EntryLine({ id, description, value, isExpense = false, deleteEntry }) {
+    const [isOpen, setIsOpen] = useState(false);
     return (
-        <Segment color={isExpense ? "red" : "green"}>
-            <Grid columns={3} textAlign="right">
-                <Grid.Row>
-                    <Grid.Column columns={10} textAlign="left">{description}</Grid.Column>
-                    <Grid.Column columns={3} textAlign="right">{value}</Grid.Column>
-                    <Grid.Column columns={3}>
-                        <Icon name="edit" bordered />
-                        <Icon name="trash" bordered onClick={() => deleteEntry(id)} />
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-        </Segment>
+        <Fragment>
+            <Segment color={isExpense ? "red" : "green"}>
+                <Grid columns={3} textAlign="right">
+                    <Grid.Row>
+                        <Grid.Column columns={10} textAlign="left">{description}</Grid.Column>
+                        <Grid.Column columns={3} textAlign="right">{value}</Grid.Column>
+                        <Grid.Column columns={3}>
+                            <Icon name="edit" bordered onClick={() => setIsOpen(true)} />
+                            <Icon name="trash" bordered onClick={() => deleteEntry(id)} />
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Segment>
+            <ModalEdit isOpen={isOpen} setIsOpen={setIsOpen} />
+        </Fragment>
     );
 } export default EntryLine;
---
+++ src/components/ModalEdit.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Button, Modal } from 'semantic-ui-react';
+import NewEntryForm from './NewEntryForm';
+
+function ModalEdit({ isOpen, setIsOpen }) {
+    return (
+        <Modal open={isOpen}>
+            <Modal.Header>Edit entry</Modal.Header>
+            <Modal.Content>
+                <NewEntryForm />
+            </Modal.Content>
+            <Modal.Actions>
+                <Button onClick={() => setIsOpen(false)}>Close</Button>
+            </Modal.Actions>
+        </Modal>
+    );
+} export default ModalEdit;
