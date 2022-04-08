a627fb28003faa74e0e108acc4da275729532fb7
Steven <stevengf14@gmail.com>
2022-04-08 16:26:34 -0500

    Making our components more reusable

---
+++ src/components/EntryForm.js
@@ -0,0 +1,38 @@
+import React, { Fragment } from "react";
+import { Form, Segment, Checkbox } from 'semantic-ui-react';
+
+function EntryForm({
+    description, value, isExpense, setDescription, setValue, setIsExpense
+}) {
+    return (
+        <Fragment>
+            <Form.Group>
+                <Form.Input
+                    icon="tags"
+                    width={12}
+                    label="Description"
+                    placholder="New shinny thing"
+                    value={description}
+                    onChange={(event) => setDescription(event.target.value)}
+                />
+                <Form.Input
+                    icon="dollar"
+                    width={4}
+                    label="Value"
+                    placholder="100.00"
+                    iconPosition="left"
+                    value={value}
+                    onChange={(event) => setValue(event.target.value)}
+                />
+            </Form.Group>
+            <Segment compact>
+                <Checkbox
+                    toggle
+                    lable="is expense"
+                    checked={isExpense}
+                    onChange={() => setIsExpense(oldState => !oldState)}
+                />
+            </Segment>
+        </Fragment>
+    );
+} export default EntryForm;
--- src/components/NewEntryForm.js
+++ src/components/NewEntryForm.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Form, Segment, Checkbox } from 'semantic-ui-react';
+import { Form } from 'semantic-ui-react';
 import ButtonSaveCancel from './ButtonSaveCancel';
 import { useState } from 'react';
+import EntryForm from './EntryForm';
 
 function NewEntryForm({ createEntry }) {
 
@@ -10,33 +11,13 @@ function NewEntryForm({ createEntry }) {
     const [isExpense, setIsExpense] = useState(true);
     return (
         <Form unstackable>
-            <Form.Group>
-                <Form.Input
-                    icon="tags"
-                    width={12}
-                    label="Description"
-                    placholder="New shinny thing"
-                    value={description}
-                    onChange={(event) => setDescription(event.target.value)}
-                />
-                <Form.Input
-                    icon="dollar"
-                    width={4}
-                    label="Value"
-                    placholder="100.00"
-                    iconPosition="left"
-                    value={value}
-                    onChange={(event) => setValue(event.target.value)}
-                />
-            </Form.Group>
-            <Segment compact>
-                <Checkbox
-                    toggle
-                    lable="is expense"
-                    checked={isExpense}
-                    onChange={() => setIsExpense(oldState => !oldState)}
-                />
-            </Segment>
+            <EntryForm description={description}
+                value={value}
+                isExpense={isExpense}
+                setDescription={setDescription}
+                setValue={setValue}
+                setIsExpense={setIsExpense}
+            />
             <ButtonSaveCancel
                 createEntry={createEntry}
                 description={description}
